8d0b114da02a904b9c386bc17b7a334809ea62ec
khilller <akhil.hem@outlook.com>
2021-05-18 13:32:26 -0400

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,40 @@
+let myImage = document.querySelector('img')
+
+myImage.onclick = function(){
+    let mySrc = myImage.getAttribute('src')
+    if(mySrc === 'images/noun_ar_2300882.png'){
+        myImage.setAttribute('src', 'images/noun_ar_2304389.png')
+    } else {
+        myImage.setAttribute('src','images/noun_ar_2300882.png')
+    }
+}
+
+let myButton = doc.querySelector('button');
+let myHeading = doc.querySelector('h1');
+
+function setUserName(){
+    let myName = prompt('Please enter your name.');
+    localStorage.setItem('name',myName);
+    myHeading.textcontent = 'XR is cool, ' + myName;
+}
+
+if (!localStorage.getItem('name')){
+    setUserName();
+} else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textcontent = 'XR is cool ' + storedName;
+}
+
+myButton.onclick = function(){
+    setUserName
+}
+
+function setUserName() {
+    let myName = prompt('Please enter your name.');
+    if(!myName) {
+      setUserName();
+    } else {
+      localStorage.setItem('name', myName);
+      myHeading.textContent = 'Mozilla is cool, ' + myName;
+    }
+  }
